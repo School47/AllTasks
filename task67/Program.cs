e8114cac15c3ee8ee2f2f15168f39c720db3af77
School47 <School47@gmail.com>
2023-04-20 14:16:15 +0500

    63 65 67 69

---
+++ task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое положительное число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+int totalResult = SummOfDigitsOfNumber(number);
+Console.WriteLine(totalResult);
+
+int SummOfDigitsOfNumber(int num)
+{
+    return (num == 0)? 0 : num % 10 + SummOfDigitsOfNumber(num / 10);
+}
---
+++ task69/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+Console.WriteLine("Введите целое положительное число A (основание степени):");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число B (степень):");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB >= 0)
+{
+    int totalResult = RaiseToDegree(numberA, numberB);
+    Console.WriteLine(totalResult);
+}
+else Console.WriteLine("B должно быть целым неотрицательным!");
+
+int RaiseToDegree(int a, int b)
+{
+   return (b == 0)? 1: a * RaiseToDegree(a, b - 1);
+}
